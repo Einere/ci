b74a358591abbb717c8a0f630a3a42e3854e614c
Einere <kjwsx23@gamil.com>
2018-07-25 13:21:56 +0900

    Implement login, logout page

---
+++ application/controllers/Hello.php
@@ -0,0 +1,12 @@
+<?php  
+defined('BASEPATH') OR exit('No direct script access allowed');  
+  
+class Hello extends CI_Controller {  
+  
+    public function newfunction()  
+    {  
+        $this->load->view('hello_world');
+        $this->load->helper('url');
+    } 
+}  
+?>  
